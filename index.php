8136599b47bf46ee11e1df8866593edade102479
mohamedloul1 <loulmohamed346@gmail.com>
2022-06-07 15:07:39 +0200

    projectmap
    Dit is een projectmap

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="nl">
+  <head>
+    <meta charset="utf-8">
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Css -->
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/home.css">
+
+    <title>Bootstrap demo</title>
+  </head>
+  <!-- Het begin van Body -->
+  <?php include("./inclouds/navbar.php"); ?>
+  <body>
+
+      <section class="section-homepage container">
+          <div class="achtergrond">
+          <img src="./img/achtergrond.jpeg" class="rounded mx-auto d-block" alt="">
+          </div>
+
+      </section>
+    <!-- Javascript links -->
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
